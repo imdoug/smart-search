86453f3b13cb8ee251e56236d1614e5e09b1b2d0
Doug Moreira <dougmoreira@Dougs-MBP.lan>
2022-05-28 20:18:38 -0400

    footer ok, navbar in process

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import { Container } from '@mui/material'
+import { CopyrightOutlined } from '@mui/icons-material'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Container style={{display: 'flex', alignItems: 'center', fontSize: 18, color: '#999', justifyContent: 'center'}}>
+         <CopyrightOutlined style={{marginRight: 5}}/> 2022 Doug Moreira. All rights reserved.
+    </Container>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
-
+import { Container, Link } from '@mui/material'
+import { Instagram, GitHub, LinkedIn } from '@mui/icons-material'
+import { socials } from '../data/socials'
+ 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+      <Container style={{width: '100%', height: 50, display: 'flex', alignItems: 'center', justifyContent:'end'}}>
+            <Container style={{ width: 200, display: 'flex', justifyContent: 'space-between', margin : 0}}>
+                  {socials.map((value)=>(
+                        <Link key={value.id}  href={value.link}>
+                              {value.social === 'linkedin' && <LinkedIn style={{fontSize: 35, color: '#999'}}/> }
+                              {value.social === 'github' && <GitHub style={{fontSize: 35, color: '#999'}}/> }
+                              {value.social === 'instagram' && <Instagram style={{fontSize: 35, color: '#999'}}/> }
+                        </Link>
+                  ))}
+            </Container>
+            <img style={{height: 40, width: 40, margin: 5, borderRadius: 50 , backgroundColor: '#FFF'}}src={'https://i.pinimg.com/originals/10/b7/f5/10b7f5c9d257a8e7569a04dc596fc5ca.jpg'} alt={'darth vader'}/>
+      </Container>
   )
 }
 
